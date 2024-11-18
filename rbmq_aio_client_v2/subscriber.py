bb1233f0894ab41350b14ee192a30e7a51dede1e
Rashmi <rashmi@ip-192-168-1-5.ap-south-1.compute.internal>
2024-11-18 20:19:51 +0530

    [chore][rbmq] publisher and subscriber async changes and test scripts

---
+++ rbmq_aio_client_v2/subscriber.py
@@ -0,0 +1,124 @@
+import json
+import ast
+import addict
+import asyncio
+import aio_pika
+import aio_pika.abc
+import pprint
+import time
+import traceback
+from typing import Union
+from structlog import get_logger
+from typing import Callable
+
+class Subscriber:
+    
+    def __init__(self, config: "dict", callback: "Callable", debug=True, expose_connection=False):
+        self.__config = addict.Dict(config)
+        self.__debug = debug
+        self.__callback = callback
+        self.__logger = get_logger()
+        self.__expose_connection = expose_connection
+        self.__failure_sleep_counter = 0
+
+    async def main(self, loop, connection_type: "Union[str, aio_pika.RobustConnection]", queue: "str"): 
+
+        try:
+            if isinstance(connection_type, str):
+                connection_args = self.__config.connections.get(connection_type)
+                connection: aio_pika.RobustConnection = await aio_pika.connect_robust(connection_args.uri, 
+                                                                                    loop=loop, 
+                                                                                    timeout=connection_args.timeout)
+            elif isinstance(connection_type, aio_pika.RobustConnection):
+                connection = connection_type
+            else:
+                raise Exception("Invalid Connection Type")
+            
+            queue_args = self.__config.queues.get(queue)
+            exchange_args = self.__config.exchanges.get(queue_args.exchange)
+            
+            if self.__debug:
+                if isinstance(connection_type, str) and isinstance(connection_args, dict):
+                    self.__logger.debug(f"ConnectionProfile: {connection_args.uri}")
+                    
+                for key, value in queue_args.items():
+                    self.__logger.debug(f"QueueProfile: {key}={value}")
+                for key, value in exchange_args.items():
+                    self.__logger.debug(f"QueueProfile: {key}={value}")
+            
+            self.__logger.info("Connection Established")
+            channel: aio_pika.abc.AbstractChannel = await connection.channel()
+            
+            self.__logger.info("Channel Established")
+            exchange: aio_pika.Exchange = await channel.declare_exchange(exchange_args.name, 
+                                                                        exchange_args.type, 
+                                                                        durable=exchange_args.durable,
+                                                                        auto_delete=exchange_args.auto_delete,
+                                                                        internal=exchange_args.internal,
+                                                                        passive=exchange_args.passive,
+                                                                        timeout=exchange_args.timeout)
+            self.__logger.info("Exchange Declared")
+            # exchange = await channel.get_exchange(exchange_args.name)
+            async with connection:
+                # Creating channel
+                channel: aio_pika.abc.AbstractChannel = await connection.channel()
+                # Declaring queue
+                queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(queue_args.name,
+                                                                                durable=(queue_args.durable or False),
+                                                                                auto_delete=queue_args.auto_delete)
+                self.__logger.info("Queue Declared")
+                
+                await queue.bind(exchange,
+                                routing_key=queue_args.routing_key)
+                self.__logger.info("Queue Bound")
+                
+                async with queue.iterator() as queue_iter:
+                    # Cancel consuming after __aexit__
+                    async for message in queue_iter:
+                        message_info = message.info()
+                        self.__logger.info("Message received", id=message_info.get('message_id'))
+                    
+                        if self.__debug:
+                            for key, value in message_info.items():
+                                self.__logger.debug("Message Info", key=key, value=value, id=message_info.get('message_id'))
+                        try:
+                            async with message.process(requeue=True):
+                                try:
+                                    payload = json.loads(message.body.decode())
+                                except:
+                                    payload = ast.literal_eval(message.body.decode())
+                                
+                                if self.__debug:
+                                    self.__logger.debug(pprint.pformat(payload), 
+                                                        id=message_info.get('message_id'))
+                                if self.__expose_connection == True:
+                                    self.__callback(payload, 
+                                                    connection=connection, 
+                                                    loop=loop,
+                                                    exchange=exchange)
+                                else:
+                                    self.__callback(payload)
+                                
+                                self.__logger.info("Message processed successfully", id=message_info.get('message_id'))
+                        except Exception as e:
+                            print(e)
+                            raise e
+        except Exception as e:
+            print(e)
+            self.__logger.error(e)
+            self.__logger.debug(traceback.format_exc())
+            self.__failure_sleep_counter += 1
+            self.__logger.debug("sleeping for {} before recreating connection".format(self.__failure_sleep_counter * 3))
+            time.sleep(self.__failure_sleep_counter * 3)
+
+    async def run(self, connection, queue):
+        while True:
+            try:
+                loop = asyncio.get_event_loop()
+                if not loop:
+                    loop = asyncio.new_event_loop()
+                loop.run_until_complete(await self.main(loop, connection, queue))
+                loop.close()
+            except Exception as e:
+                traceback.print_exc()
+                print(e)
---
+++ rbmq_aio_client_v2/test_subscriber.py
@@ -0,0 +1,71 @@
+
+import click
+import json
+import traceback
+import threading
+import asyncio
+from rbmq_aio_client_v2.subscriber import Subscriber
+from rbmq_aio_client_v2.server import HealthCheckServer
+from rbmq_aio_client_v2.test_publisher import config
+# from app.env import rabbitmq_conf_v2
+# from app.events.paytm import PaytmWebhook
+from structlog import get_logger
+
+rabbitmq_conf_v2 = config
+
+class PaytmWebhook:
+    def process(self, data):
+        print("data", data)
+
+actions_dict = {
+    'PAYTM_WEBHOOK1': PaytmWebhook()
+}
+
+LOGGER = get_logger()
+
+
+
+@click.command()
+@click.option("--connection")
+@click.option("--queue")
+@click.option("--health_port", default="8000")
+@click.option("--debug", default="false")
+def run(connection: "str", queue: "str", health_port: "str", debug: "str"):
+    async def run_async():
+        # Start HealthCheckServer
+        print("HERE")
+        print(connection, queue)
+        # HealthCheckServer(port=int(health_port), log=True).start()
+        
+        # Define on_message callback function
+        def on_message(payload: dict):
+            try:
+                if isinstance(payload, str):
+                    payload = json.loads(payload)
+
+                action = payload.get('action')
+                attributes = payload.get('attributes')
+                print("Action:", action)
+                print("Attributes:", attributes)
+
+                action_func = actions_dict.get(action)
+                if not action_func:
+                    LOGGER.warning("[action] not setup", action=action, payload=payload)
+                    return
+
+                action_func.process(attributes)
+            except Exception as e:
+                print("Error processing message:", e)
+                traceback.print_exc()
+        
+        # Initialize and run Subscriber
+        await Subscriber(
+            rabbitmq_conf_v2, on_message, debug=(True if debug == "true" else False)
+        ).run(connection, queue)
+
+    # Run the asynchronous code
+    asyncio.run(run_async())
+
+
+if __name__ == '__main__':
+    run()
