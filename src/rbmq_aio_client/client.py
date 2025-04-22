85c1f7da755dbe0e68dd2b51c4ecd6108cd279c4
Nirbhay Gupta <nirbhay.gupta@junio.in>
2025-04-22 12:45:47 +0530

    [fix][client] publisher stoppage event

--- src/rbmq_aio_client/client.py
+++ src/rbmq_aio_client/client.py
@@ -14,6 +14,7 @@ from typing import Tuple, List, Awaitable, Any, Callable
 
 logger = get_logger(__name__)
 
+
 @dataclass
 class ConnectionConfig:
     connection_uri: str
@@ -22,6 +23,7 @@ class ConnectionConfig:
     channel_max_count: int
     queue_max_size: int
     debug: bool = False
+
 
 @dataclass
 class ExchangeConfig:
@@ -33,6 +35,7 @@ class ExchangeConfig:
     passive: bool
     timeout: int
 
+
 @dataclass
 class QueueConfig:
     name: str
@@ -41,6 +44,7 @@ class QueueConfig:
     exclusive: bool
     routing_keys: List[str]
     prefetch_count: int = 10
+
 
 class RBMQAsyncioClient:
 
@@ -53,22 +57,25 @@ class RBMQAsyncioClient:
         self.__debug: bool = config.debug
 
     async def get_connection(self) -> aio_pika.abc.AbstractConnection:
-        return await aio_pika.connect_robust(self.__connection_uri, 
-                                             timeout=self.__connection_timeout)
+        return await aio_pika.connect_robust(
+            self.__connection_uri, timeout=self.__connection_timeout
+        )
 
     async def get_channel(self) -> aio_pika.abc.AbstractChannel:
-        async with self.__connection_pool.acquire() as connection: # type: aio_pika.abc.AbstractConnection
+        async with self.__connection_pool.acquire() as connection:  # type: aio_pika.abc.AbstractConnection
             return await connection.channel()
-        
+
     @asynccontextmanager
     async def setup(self):
         self.__message_queue = asyncio.queues.Queue(maxsize=self.__queue_max_size)
-        self.__connection_pool = aio_pika.pool.Pool(self.get_connection, 
-                                                    max_size=self.__connection_max_count)
+        self.__connection_pool = aio_pika.pool.Pool(
+            self.get_connection, max_size=self.__connection_max_count
+        )
 
-        self.__channel_pool = aio_pika.pool.Pool(self.get_channel, 
-                                                 max_size=self.__channel_max_count)
-        
+        self.__channel_pool = aio_pika.pool.Pool(
+            self.get_channel, max_size=self.__channel_max_count
+        )
+
         yield
 
         await self.__channel_pool.close()
@@ -76,134 +83,178 @@ class RBMQAsyncioClient:
 
     async def configure(self):
         self.__message_queue = asyncio.queues.Queue(maxsize=self.__queue_max_size)
-        self.__connection_pool = aio_pika.pool.Pool(self.get_connection, 
-                                                    max_size=self.__connection_max_count)
+        self.__connection_pool = aio_pika.pool.Pool(
+            self.get_connection, max_size=self.__connection_max_count
+        )
 
-        self.__channel_pool = aio_pika.pool.Pool(self.get_channel, 
-                                                 max_size=self.__channel_max_count)
-        
+        self.__channel_pool = aio_pika.pool.Pool(
+            self.get_channel, max_size=self.__channel_max_count
+        )
+
     async def destroy(self):
-        if self.__publisher_running and self.__message_queue.qsize() > 0:
-            shutdown_event = asyncio.Event()
-            await self.__message_queue.put(shutdown_event)
-            await shutdown_event.wait()
+        self.__subscriber_running = False
         self.__publisher_running = False
+
+        if self.__publisher_running:
+            await self.__message_queue.put("stop")
+            await self.__publisher_stop_event.wait()
+
+        if self.__subscriber_running:
+            await self.__subscriber_stop_event.wait()
+
         await self.__channel_pool.close()
         await self.__connection_pool.close()
 
-
-    async def create_publisher(self, exchange_config: ExchangeConfig):
-        __close_event: asyncio.Event = None
+    async def create_publisher(
+        self, exchange_config: ExchangeConfig, stop_event: asyncio.Event
+    ):
         delay_counter = 0
         while self.__publisher_running:
             try:
                 async with self.__connection_pool, self.__channel_pool:
                     if self.__channel_pool.is_closed:
                         return
-                    
-                    async with self.__channel_pool.acquire() as ch: # type: aio_pika.abc.AbstractChannel
+
+                    async with self.__channel_pool.acquire() as ch:  # type: aio_pika.abc.AbstractChannel
                         channel: aio_pika.abc.AbstractChannel = ch
-                        exchange: aio_pika.abc.AbstractExchange = await channel.declare_exchange(
-                            name=exchange_config.name,
-                            type=exchange_config.type,
-                            durable=exchange_config.durable,
-                            auto_delete=exchange_config.auto_delete,
-                            internal=exchange_config.internal,
-                            passive=exchange_config.passive,
-                            timeout=exchange_config.timeout
+                        exchange: aio_pika.abc.AbstractExchange = (
+                            await channel.declare_exchange(
+                                name=exchange_config.name,
+                                type=exchange_config.type,
+                                durable=exchange_config.durable,
+                                auto_delete=exchange_config.auto_delete,
+                                internal=exchange_config.internal,
+                                passive=exchange_config.passive,
+                                timeout=exchange_config.timeout,
+                            )
                         )
                         while self.__publisher_running:
-                            item: Tuple[aio_pika.Message, str, int] | asyncio.Event = await self.__message_queue.get()
-                            
-                            if isinstance(item, asyncio.Event):
-                                __close_event = item
-                                raise Exception("stop-publisher")                                
+                            item: Tuple[aio_pika.Message, str, int] | str = (
+                                await self.__message_queue.get()
+                            )
+
+                            if isinstance(item, str) and item == "stop":
+                                raise Exception("stop-publisher")
 
                             message = item[0]
                             routing_key = item[1]
                             publish_timeout = item[2]
                             confirmation = await exchange.publish(
-                                message,
-                                routing_key,
-                                timeout=publish_timeout
+                                message, routing_key, timeout=publish_timeout
                             )
-                            await logger.ainfo(f"message-delivery: {message.message_id}: {confirmation.delivery_tag}")
+                            await logger.ainfo(
+                                f"message-delivery: {message.message_id}: {confirmation.delivery_tag}"
+                            )
 
             except Exception as e:
                 if str(e) == "stop-publisher":
-                    await logger.info("closing publisher")
-                    __close_event.set()
-                    self.__publisher_running = False
-                
+                    break
+
                 logger.error(e, exc_info=self.__debug)
-                if self.__publisher_running == True:
-                    delay_counter += 1
-                    await asyncio.sleep(delay_counter * 3)
+                delay_counter += 1
+                await asyncio.sleep(delay_counter * 3)
+        stop_event.set()
 
-    async def create_subscriber(self, exchange_config: ExchangeConfig, queue_config: QueueConfig, callback: Callable[[Any], Awaitable[Any]]):
+    async def create_subscriber(
+        self,
+        exchange_config: ExchangeConfig,
+        queue_config: QueueConfig,
+        callback: Callable[[Any], Awaitable[Any]],
+        stop_event: asyncio.Event,
+    ):
         delay_counter = 0
-        try:
-            async with self.__connection_pool, self.__channel_pool:
-                async with self.__channel_pool.acquire() as ch: # type: aio_pika.abc.AbstractChannel
-                    channel: aio_pika.abc.AbstractChannel = ch
-                    exchange: aio_pika.abc.AbstractExchange = await channel.declare_exchange(
-                        name=exchange_config.name,
-                        type=exchange_config.type,
-                        durable=exchange_config.durable,
-                        auto_delete=exchange_config.auto_delete,
-                        internal=exchange_config.internal,
-                        passive=exchange_config.passive,
-                        timeout=exchange_config.timeout
-                    )
-                    queue: aio_pika.Queue = await channel.declare_queue(
-                        name=queue_config.name,
-                        durable=queue_config.durable,
-                        auto_delete=queue_config.auto_delete,
-                        exclusive=queue_config.exclusive
-                    )
-                    await channel.set_qos(prefetch_count=queue_config.prefetch_count)
-                    for routing_key in queue_config.routing_keys:
-                        await queue.bind(exchange=exchange, routing_key=routing_key)
+        while self.__subscriber_running:
+            try:
+                async with self.__connection_pool, self.__channel_pool:
+                    async with self.__channel_pool.acquire() as ch:  # type: aio_pika.abc.AbstractChannel
+                        channel: aio_pika.abc.AbstractChannel = ch
+                        exchange: aio_pika.abc.AbstractExchange = (
+                            await channel.declare_exchange(
+                                name=exchange_config.name,
+                                type=exchange_config.type,
+                                durable=exchange_config.durable,
+                                auto_delete=exchange_config.auto_delete,
+                                internal=exchange_config.internal,
+                                passive=exchange_config.passive,
+                                timeout=exchange_config.timeout,
+                            )
+                        )
+                        queue: aio_pika.Queue = await channel.declare_queue(
+                            name=queue_config.name,
+                            durable=queue_config.durable,
+                            auto_delete=queue_config.auto_delete,
+                            exclusive=queue_config.exclusive,
+                        )
+                        await channel.set_qos(
+                            prefetch_count=queue_config.prefetch_count
+                        )
+                        for routing_key in queue_config.routing_keys:
+                            await queue.bind(exchange=exchange, routing_key=routing_key)
 
-                    async with queue.iterator() as queue_iter:
-                        async for message in queue_iter:
-                            try:
-                                async with message.process(ignore_processed=True):
-                                    try:
-                                        payload = json.loads(message.body.decode())
-                                    except json.JSONDecodeError as e:
-                                        payload = ast.literal_eval(message.body.decode())
-                                    except:
-                                        await message.reject()
+                        async with queue.iterator() as queue_iter:
+                            async for message in queue_iter:
+                                if self.__subscriber_running == False:
+                                    raise Exception("stop-subscriber")
 
-                                    try:
-                                        ack = await callback(payload)
-                                        if ack:
-                                            await message.ack()
-                                        else:
+                                try:
+                                    async with message.process(ignore_processed=True):
+                                        try:
+                                            payload = json.loads(message.body.decode())
+                                        except json.JSONDecodeError as e:
+                                            payload = ast.literal_eval(
+                                                message.body.decode()
+                                            )
+                                        except:
                                             await message.reject()
-                                    except Exception as e:
-                                        logger.error(e)
-                                        await message.reject(requeue=True)
-                            except Exception as e:
-                                logger.error(e, exc_info=self.__debug)
-        except Exception as e:
-            logger.error(e, exc_info=self.__debug)
-            delay_counter += 1
-            await asyncio.sleep(delay_counter * 3)
+
+                                        try:
+                                            ack = await callback(payload)
+                                            if ack:
+                                                await message.ack()
+                                            else:
+                                                await message.reject()
+                                        except Exception as e:
+                                            logger.error(e)
+                                            await message.reject(requeue=True)
+                                except Exception as e:
+                                    logger.error(e, exc_info=self.__debug)
+            except Exception as e:
+                if str(e) == "stop-subscriber":
+                    break
+                logger.error(e, exc_info=self.__debug)
+                delay_counter += 1
+                await asyncio.sleep(delay_counter * 3)
+        stop_event.set()
 
     async def run_publisher(self, config: ExchangeConfig):
         self.__publisher_running = True
+        self.__publisher_stop_event = asyncio.Event()
         loop = asyncio.get_running_loop()
-        return loop.create_task(self.create_publisher(config))
+        return loop.create_task(
+            self.create_publisher(config, stop_event=self.__publisher_stop_event)
+        )
 
-    async def run_subscriber(self, exchange_config: ExchangeConfig, queue_config: QueueConfig, callback: Callable[[Any], Awaitable[Any]]):
+    async def run_subscriber(
+        self,
+        exchange_config: ExchangeConfig,
+        queue_config: QueueConfig,
+        callback: Callable[[Any], Awaitable[Any]],
+    ):
+        self.__subscriber_running = True
+        self.__subscriber_stop_event = asyncio.Event()
         loop = asyncio.get_running_loop()
-        return loop.create_task(self.create_subscriber(exchange_config, queue_config, callback))
+        return loop.create_task(
+            self.create_subscriber(
+                exchange_config,
+                queue_config,
+                callback,
+                stop_event=self.__subscriber_stop_event,
+            )
+        )
 
     async def run_healthcheck_server(self, port: int = 8000):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('localhost', port))
+        server.bind(("localhost", port))
         server.listen(8)
         server.setblocking(False)
 
@@ -233,7 +284,5 @@ class RBMQAsyncioClient:
             reply_to=reply_queue,
         )
         await self.__message_queue.put((message, routing_key, publish_timeout))
-        
-        return self
 
-    
+        return self
